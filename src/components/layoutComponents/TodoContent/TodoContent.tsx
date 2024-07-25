d7cbb3458c6e74f3c59a9fd4154addc0187f1c31
Peester <oliversladkovsky@gmail.com>
2024-07-25 23:33:54 +0200

    TodoContent + TodoDetails styles

--- src/components/layoutComponents/TodoContent/TodoContent.tsx
+++ src/components/layoutComponents/TodoContent/TodoContent.tsx
@@ -1,26 +1,88 @@
 import styles from "./todocontent.module.scss";
 import { GoSun } from "react-icons/go";
 import { MdOutlineNightlight } from "react-icons/md";
+import { IoIosMore } from "react-icons/io";
+import { Add, Flag } from "iconsax-react";
+import classNames from "classnames";
 
 const TodoContent = () => {
   return (
     <div className={styles.todoContent_wrapper}>
       {/* Todo Content Header */}
       <div className={styles.todoContent_header}>
-        <p className={styles.todoContent_header_text}>My new category</p>
-        <MdOutlineNightlight
-          className={styles.todoContent_header_darkmode_icon}
-          size={24}
-        />
+        <p className={styles.todoContent_header_text}>My first category</p>
+        <GoSun className={styles.todoContent_header_darkmode_icon} size={24} />
       </div>
 
       {/* Todo Content form to add todo */}
 
-      <div className={styles.todoContent_form_wrapper}>
-        
+      <div className={styles.todoContent_form}>
+        <form action="">
+          <Add className={styles.todoContent_form_icon} size={20} />
+          <input
+            className={styles.todoContent_input}
+            type="text"
+            placeholder="Add new todo"
+          />
+          <input
+            className={styles.todoContent_form_submit}
+            type="submit"
+            value="Pridať todo"
+          />
+        </form>
+      </div>
+
+      {/* Todo Content tText*/}
+
+      <div className={styles.todoContent_todos}>
+        <p>Todos</p>
       </div>
 
       {/* Todo Content List */}
+
+      <div className={styles.todoContent_list_wrapper}>
+        {/* Todo Content item 1 */}
+
+        <div className={styles.todoContent_list_item}>
+          <div className={styles.todoContent_list_item_left}>
+            <input
+              className={styles.todoContent_list_item_checkbox}
+              type="checkbox"
+            />
+
+            <p className={styles.todoContent_list_item_text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia...
+            </p>
+          </div>
+
+          <div className={styles.todoContent_list_item_right}>
+            <Flag size={20} className={styles.flag_icon_priority_medium} />
+            <IoIosMore size={20} />
+          </div>
+        </div>
+
+        {/* Todo Content item 2 */}
+
+        <div className={classNames(styles.todoContent_list_item, styles.checked)}>
+          <div className={styles.todoContent_list_item_left}>
+            <input
+              className={styles.todoContent_list_item_checkbox}
+              type="checkbox"
+            />
+
+            <p className={classNames(styles.todoContent_list_item_text,styles.checked)}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia...
+            </p>
+          </div>
+
+          <div className={styles.todoContent_list_item_right}>
+            <Flag size={20} className={styles.flag_icon_priority_medium} />
+            <IoIosMore size={20} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/layoutComponents/TodoDetails/TodoDetails.tsx
+++ src/components/layoutComponents/TodoDetails/TodoDetails.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import styles from './tododetails.module.scss'
+import React from "react";
+import styles from "./tododetails.module.scss";
+import { Flag, Calendar } from "iconsax-react";
 
 const TodoDetails = () => {
   return (
-    <aside className={styles.todo_details_wrapper}>TodoDetails</aside>
-  )
-}
+    <aside className={styles.todo_details_wrapper}>
+      {/* Todo Details Header */}
+      <div className={styles.todo_details_header}>
+        <div className={styles.todo_details_header_left}>
+          <input
+            className={styles.todo_details_header_checkbox}
+            type="checkbox"
+            size={24}
+          />
+          <div className={styles.todo_details_header_divider}></div>
+          <div className={styles.todo_details_header_calendar}>
+            <Calendar size={24} />
+            <p>25.7.2024</p>
+          </div>
+        </div>
+        <div className={styles.todo_details_header_right}>
+          <Flag size={24} />
+          <p>Priority</p>
+        </div>
+      </div>
 
-export default TodoDetails
+      {/* Todo Details Content */}
+
+      <div className={styles.todo_details_content_wrapper}>
+        <p className={styles.todo_details_content_title}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia...
+        </p>
+
+        <p className={styles.todo_details_content_description}>Write some description..</p>
+      </div>
+    </aside>
+  );
+};
+
+export default TodoDetails;
